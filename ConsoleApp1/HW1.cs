3e8e43b77ae76e966900ccc7370e4868d6ea5c45
eh102 <eh102@LAPTOP-BECQ4R7L>
2022-03-16 16:12:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/HW1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class HW1
+    {
+        public static long QueueTime(int[] consumer, int n)
+        {
+            int[] kassa = new int[n];
+
+            int T = 0;
+            do
+            {
+                int s = 0;
+                for (int i = 0; i < consumer.Length; i++) s += consumer[i];
+                if (s == 0) break;
+                for (int i = 0; i < n; i++) kassa[i] = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (consumer[i] == 0) kassa[i] = 1;
+                }
+                for (int i = 0; i < kassa.Length; i++)
+                {
+                    if (kassa[i] == 1)
+                    {
+                        consumer[i] = consumer[n];
+                        for (int j = n; j < consumer.Length - 1; j++)
+                        {
+                            consumer[j] = consumer[j + 1];
+                        }
+                        consumer[consumer.Length - 1] = 0;
+                    }
+                    if (consumer[i] != 0)
+                        consumer[i]--;
+                }
+                T++;
+            } while (true);
+            return T;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number1 = { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(number1, 1));
+            int[] number2  = { 10, 2, 3, 3 };
+            Console.WriteLine(HW1.QueueTime(number2, 2));
+            int[] number3 = { 2, 3, 10 };
+            Console.WriteLine(HW1.QueueTime(number3, 2));
+        }
+    }
+}
